5c4c55b891653068ee1bab1edb69dec811277a1d
saas-nori <sasanori.0126@gmail.com>
2020-05-02 15:22:11 +0900

    fix danger lint error

--- example/src/main/java/net/newstyleservice/example/MainActivity.kt
+++ example/src/main/java/net/newstyleservice/example/MainActivity.kt
@@ -53,7 +53,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
