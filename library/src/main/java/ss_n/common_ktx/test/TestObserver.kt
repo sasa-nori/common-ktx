c2e203b0d2ac81962662838d826293b2b26acf7c
sasa-nori <sasanori.0126@gmail.com>
2023-09-18 22:40:18 +0900

    fix onChanged params

--- library/src/main/java/ss_n/common_ktx/test/TestObserver.kt
+++ library/src/main/java/ss_n/common_ktx/test/TestObserver.kt
@@ -7,7 +7,7 @@ class TestObserver<T> : Observer<T> {
 
     val observedValues = mutableListOf<T?>()
 
-    override fun onChanged(value: T?) {
+    override fun onChanged(value: T) {
         observedValues.add(value)
     }
 
